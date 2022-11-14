1f43537bf878451adc5f6793a09ab19d14cc837a
13angs <13angz.um@gmail.com>
2022-11-14 23:12:52 +0700

    se-backend: convert in-mem to mongodb comment-sv

--- backend/apps/comment-sv/Services/CommentService.cs
+++ backend/apps/comment-sv/Services/CommentService.cs
@@ -1,26 +1,27 @@
 using AutoMapper;
 using comment_sv.Interfaces;
-using comment_sv.Models;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using SeBackend.Common.DTOs;
 using SeBackend.Common.Interfaces;
 using SeBackend.Common.Models;
+using SeBackend.Common.Services;
 
 namespace comment_sv.Services
 {
   public class CommentService : ControllerBase, ICommentService
   {
-    private readonly CommentContext context;
     private readonly ILogger<CommentService> logger;
     private readonly IMessagePublisher publisher;
     private readonly IConfiguration configuration;
     private readonly IMapper mapper;
 
-    public CommentService(CommentContext context, ILogger<CommentService> logger, 
+    private readonly IMongoCollection<Comment?> _comment;
+
+    public CommentService(ILogger<CommentService> logger, 
         IMessagePublisher publisher, IConfiguration configuration, IMapper mapper)
     {
-      this.context = context;
       this.logger = logger;
       this.publisher = publisher;
       this.configuration = configuration;
@@ -29,23 +30,19 @@ namespace comment_sv.Services
         configuration["RabbitMQ:HostName"],
         "report_se_exchange"
       );
+      _comment = MongodbCollectionService.GetCollection<Comment?>(configuration);
     }
     public async Task<ActionResult> Post(Comment comment)
     {
       try
       {
-        context.Comments.Add(comment);
-        int result = await context.SaveChangesAsync();
-        if(result>0)
-        {
-            logger.LogInformation("--> Comment created!");
-            ReportCommentModel commentModel = new ReportCommentModel();
-            mapper.Map<Comment, ReportCommentModel>(comment, commentModel);
-            commentModel.EventName=EventNameStore.Comment.Create;
-            publisher.Publish(JsonConvert.SerializeObject(commentModel), "report.se", null);
-            return StatusCode(StatusCodes.Status201Created);
-        }
-        return StatusCode(StatusCodes.Status500InternalServerError);
+        await _comment.InsertOneAsync(comment);
+        logger.LogInformation("--> Comment created!");
+        ReportCommentModel commentModel = new ReportCommentModel();
+        mapper.Map<Comment, ReportCommentModel>(comment, commentModel);
+        commentModel.EventName=EventNameStore.Comment.Create;
+        publisher.Publish(JsonConvert.SerializeObject(commentModel), "report.se", null);
+        return StatusCode(StatusCodes.Status201Created);
       }
       catch (Exception e)
       {
@@ -57,7 +54,7 @@ namespace comment_sv.Services
 
     public ActionResult<IEnumerable<Comment>> Get()
     {
-      IEnumerable<Comment> comments = context.Comments;
+      IEnumerable<Comment?> comments = _comment.Find(c => true).ToList();
       return Ok(comments);
     }
   }
--- backend/libs/SeBackend.Common/Models/Comment.cs
+++ backend/libs/SeBackend.Common/Models/Comment.cs
@@ -1,19 +1,26 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
 
 namespace SeBackend.Common.Models
 {
     public class Comment : BaseEntity
     {
-
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("comment_id")]
         [JsonProperty("comment_id")]
-        public int CommentId { get; set; }
+        public string? CommentId { get; set; }
 
+        [BsonElement("product_id")]
         [JsonProperty("product_id")]
-        public int ProductId { get; set; }
+        public string? ProductId { get; set; }
 
+        [BsonElement("text")]
         [JsonProperty("text")]
         public string? Text { get; set; }
 
+        [BsonElement("nlikes")]
         [JsonProperty("nlikes")]
         public int NLikes { get; set; }
     }
